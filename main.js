dbcc4d2418f2d730be1048e4e19ca7a9cc7c3e4e
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-03-09 01:42:21 -0300

    refactor: refatorei o código

---
+++ main.js
@@ -0,0 +1,9 @@
+const emailInput = document.getElementById("email")
+const passwordInput = document.getElementById("password")
+const imgEmail = document.getElementById("mail")
+const imgPassword = document.getElementById("lock")
+
+const setFocus = () => {
+  if (emailInput.focus) {
+  }
+}
