41803da4546ee700e17ab75377b54d18779fefe3
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-06-17 20:00:43 -0300

    Adicionei um toggle no input de senha

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const emailInput = document.getElementById("email")
-const passwordInput = document.getElementById("password")
-const imgEmail = document.getElementById("mail")
-const imgPassword = document.getElementById("lock")
-
-const setFocus = () => {
-  if (emailInput.focus) {
+const inputPassword = document.getElementById("password");
+const buttonVisibility = document.getElementById("eye");
+buttonVisibility.addEventListener("click", function () {
+  if (inputPassword.type === "password") {
+    inputPassword.type = "text";
+  } else {
+    inputPassword.type = "password";
   }
-}
+});
